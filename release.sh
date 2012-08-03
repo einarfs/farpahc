8bedfe075ad23ac998d1a53e595e2a88e14a7cdf
Anton Karl Ingason <anton.karl.ingason@gmail.com>
2012-08-03 11:52:25 -0400

    add files

---
+++ release.sh
@@ -0,0 +1,47 @@
+CORPUSNAME="farpahc"
+VERSION="0.1"
+DIRNAME=$CORPUSNAME"-v"$VERSION"/"
+TEMPFILE="temp.psd"
+rm -rf $DIRNAME
+mkdir $DIRNAME
+cp lgpl.txt $DIRNAME"lgpl.txt"
+cp gpl.txt $DIRNAME"gpl.txt"
+cp README $DIRNAME"README"
+
+mkdir $DIRNAME"psd"
+mkdir $DIRNAME"txt"
+mkdir $DIRNAME"tagged"
+mkdir $DIRNAME"info"
+
+echo
+echo "Copy finished psd files to release"
+cp finished/*.psd $DIRNAME"psd"
+
+echo
+echo "Copy info files into info directory"
+cp info/*.info $DIRNAME"info"
+
+echo
+echo "Generate text versions from psd versions"
+./tscripts/src/psd2text.py "finished/*.psd" $DIRNAME"txt/"
+
+echo
+echo "Generate tagged versions from psd versions"
+./tscripts/src/psd2tagged.py "finished/*.psd" $DIRNAME"tagged/"
+
+echo 
+echo "Add README file to top of each psd file"
+for i in $(ls farpahc-v0.1/psd/*.psd);
+do
+echo "/*" >> $TEMPFILE
+cat README >> $TEMPFILE
+echo "*/" >> $TEMPFILE
+cat $i >> $TEMPFILE
+mv $TEMPFILE $i
+done
+exit
+
+
+
+
+
