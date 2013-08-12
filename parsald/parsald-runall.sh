977f39596bff5aa1e8533bee352aced3d5944638
Einar Freyr Sigurðsson <einar@einar-laptop.(none)>
2013-08-12 16:22:48 +0000

    runall.sh update

--- parsald/parsald-runall.sh
+++ parsald/parsald-runall.sh
@@ -14,6 +14,26 @@ echo "Delete CODE"
 $CS structure_queries/parsald-delete-code.q $FILE > problemfile.txt
 mv -f $FILE.out $FILE
 
+echo ""
+echo "Delete ICH"
+$CS structure_queries/parsald-delete-ICH.q $FILE > problemfile.txt
+mv -f $FILE.out $FILE
+
+echo ""
+echo "Delete traces"
+$CS structure_queries/parsald-delete-traces.q $FILE > problemfile.txt
+mv -f $FILE.out $FILE
+
+echo ""
+echo "Delete 0"
+$CS structure_queries/parsald-delete-0.q $FILE > problemfile.txt
+mv -f $FILE.out $FILE
+
+echo ""
+echo "Delete empty subjects"
+$CS structure_queries/parsald-delete-empty_subjects.q $FILE > problemfile.txt
+mv -f $FILE.out $FILE
+
 python3 "scripts/rm-lemmata.py" $FILE > $FILE.out
 mv -f $FILE.out $FILE
 
