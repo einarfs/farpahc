6907d5e55c4c3d185c0c0264abe7a1ae432e5d52
Anton Karl Ingason <anton.karl.ingason@gmail.com>
2013-09-21 20:21:02 -0400

    script to run parser on split

---
+++ parsald/exp/e1/runparser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# usage: 
+# runparser.sh NUMBER
+# where number is the number of the split in the cross-val, 0-9
+split=$1
+start=$(date +%s)
+
+grammar_file="grammars/far"${split}".gr"
+input_file="corpus/far_split"${split}"testing.txt"
+output_file=$"output/far_split"${split}"machineparsed.psd"
+
+# train parser
+java -jar ../../BerkeleyParser-1.7.jar -gr $grammar_file -inputFile $input_file -outputFile $output_file
+
+end=$(date +%s)
+diff=$(( $end - $start ))
+echo "Machine parsing of test chunk $split in seconds: $diff"
+
+
+
