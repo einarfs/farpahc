17f0168b2427b11e3dd0cf8399ee544a8d89541f
einarfs <einarfs@gmail.com>
2014-03-13 16:49:29 -0400

    edit train

--- parsald/exp/ice/train.sh
+++ parsald/exp/ice/train.sh
@@ -5,10 +5,10 @@
 split=$1
 start=$(date +%s)
 
-training_file="corpus/far_split"${split}"training.psd"
-grammar_file="grammars/far"${split}".gr"
-training_report="grammars/far"${split}"report.txt"
-time_report="grammars/far"${split}"time.txt"
+training_file="corpus/icepahc09.psd"
+grammar_file="grammars/icepahc09.gr"
+training_report="grammars/icepahc09report.txt"
+time_report="grammars/icetrainingtime.txt"
 echo $training_file
 
 # train parser
@@ -16,7 +16,7 @@ java -Xmx32768M -cp ../../BerkeleyParser-1.7.jar edu.berkeley.nlp.PCFGLA.Grammar
 
 end=$(date +%s)
 diff=$(( $end - $start ))
-echo "Training on split $split in seconds: $diff" > $time_report
+echo "Training on full set in seconds: $diff" > $time_report
 
 
 
